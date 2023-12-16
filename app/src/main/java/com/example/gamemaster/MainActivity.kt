c94d4f5308be3c5bcca858f730f96f234f819708
pcismynme <pcismyname007@gmail.com>
2023-12-17 04:10:20 +0700

    update dark theme

--- app/src/main/java/com/example/gamemaster/MainActivity.kt
+++ app/src/main/java/com/example/gamemaster/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GameMasterTheme {
+            GameMasterTheme(darkTheme = true) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
